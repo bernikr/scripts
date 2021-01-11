1431d0053524922d6fbc490bb225c82edc00bb7f
Bernhard Kralofsky <bernhard@kralofsky.com>
2021-01-11 17:39:52 +0100

    first protype of the importer

--- firefly_api.py
+++ firefly_api.py
@@ -1,9 +1,25 @@
+import re
+import urllib
+
 import requests
 from requests import HTTPError
+from datetime import date
+GET = 'GET'
+POST = 'POST'
+
+API_PATH = "api/v1/"
 
 
 class APIError(Exception):
     pass
+
+
+def format_date(d):
+    if isinstance(d, str) and re.match(r'^\d{4}-(0[0-9]|1[0-2])-([012][0-9]|3[01])$', d):
+        return d
+    elif isinstance(d, date):
+        return d.isoformat()
+    raise APIError('Wrong date format, use datetime.date or YYYY-MM-DD')
 
 
 class FireflyAPI:
@@ -11,13 +27,15 @@ class FireflyAPI:
         self.base_url = base_url
         self.token = token
 
-    def request(self, url, data=None, method='GET'):
+    def _request(self, url, data=None, method=GET, args=None):
         headers = {
             'Authorization': 'Bearer ' + self.token,
             'Content-Type': 'application/json',
             'Accept': 'application/json'
         }
-        r = requests.request(method=method, url=self.base_url+url, json=data, headers=headers)
+        formatted_args = ('?' + urllib.parse.urlencode(args)) if args is not None and len(args) > 0 else ''
+        r = requests.request(method=method, url=self.base_url + API_PATH + url + formatted_args, json=data,
+                             headers=headers)
         try:
             r.raise_for_status()
         except HTTPError as e:
@@ -25,3 +43,23 @@ class FireflyAPI:
                 raise APIError(r.text)
             raise e
         return r.json()['data']
+
+    def get_about(self):
+        return self._request('about')
+
+    def get_about_user(self):
+        return self._request('about/user')
+
+    def get_account_transactions(self, account_id, start=None, end=None, page=None, limit=50):
+        args = {}
+        if start is not None:
+            args['start'] = format_date(start)
+        if end is not None:
+            args['end'] = format_date(end)
+        args['limit'] = limit
+        if page is not None:
+            args['page'] = page
+        return self._request('accounts/{}/transactions'.format(account_id), args=args)
+
+    def create_transaction(self, transaction):
+        return self._request('transactions', method=POST, data={'transactions': [transaction]})
--- n26_test.py
+++ n26_test.py
@@ -1,15 +1,64 @@
-from n26.api import Api
-from n26.config import Config
-from settings import *
+from operator import itemgetter
 
-conf = Config(validate=False)
-conf.USERNAME.value = N26_USERNAME
-conf.PASSWORD.value = N26_PASSWORD
-conf.LOGIN_DATA_STORE_PATH.value = N26_LOGIN_DATA_STORE
-conf.MFA_TYPE.value = "app"
-conf.DEVICE_TOKEN.value = N26_DEVICE_TOKEN
-conf.validate()
+import n26.config
+import n26.api
+from firefly_api import FireflyAPI
+from settings import *
+from datetime import date, datetime, timedelta
+
+n26_conf = n26.config.Config(validate=False)
+n26_conf.USERNAME.value = N26_USERNAME
+n26_conf.PASSWORD.value = N26_PASSWORD
+n26_conf.LOGIN_DATA_STORE_PATH.value = N26_LOGIN_DATA_STORE
+n26_conf.MFA_TYPE.value = "app"
+n26_conf.DEVICE_TOKEN.value = N26_DEVICE_TOKEN
+n26_conf.validate()
+
+
+def map_transaction(t, category_map):
+    transaction = {
+        'date': datetime.fromtimestamp(t['visibleTS']/1000).date().isoformat(),
+        'amount': abs(t['amount']),
+        'notes': t.get('referenceText', ''),
+        'tags': ['n26 autoimport'],
+        'internal_reference': t['id']
+    }
+    if FIREFLY_CASH_ACCOUNT_ID != -1 and t['category'] == 'micro-v2-atm':
+        transaction['type'] = 'transfer'
+        transaction['source_id'] = FIREFLY_N26_ACCOUNT_ID
+        transaction['destination_id'] = FIREFLY_CASH_ACCOUNT_ID
+        transaction['notes'] = t.get('merchantName', t.get('partnerName', ''))
+        transaction['description'] = 'ATM'
+    elif t['amount'] < 0:
+        transaction['type'] = 'withdrawal'
+        transaction['source_id'] = FIREFLY_N26_ACCOUNT_ID
+        transaction['destination_name'] = t.get('merchantName', t.get('partnerName', ''))
+        transaction['destination_iban'] = t.get('partnerIban', None)
+        transaction['category_name'] = category_map.get(t['category'], '')
+        transaction['description'] = transaction['destination_name']
+    else:
+        transaction['type'] = 'deposit'
+        transaction['destination_id'] = FIREFLY_N26_ACCOUNT_ID
+        transaction['source_name'] = t.get('merchantName', t.get('partnerName', ''))
+        transaction['source_iban'] = t.get('partnerIban', None)
+        transaction['source_name'] = transaction['destination_name']
+    return transaction
+
 
 if __name__ == '__main__':
-    api_client = Api(conf)
-    print(api_client.get_transactions())
+    n26_api = n26.api.Api(n26_conf)
+    firefly_api = FireflyAPI(FIREFLY_URL, FIREFLY_TOKEN)
+
+    last_registered_transactions = firefly_api.get_account_transactions(FIREFLY_N26_ACCOUNT_ID, limit=20)
+    last_registered_transactions = list(map(lambda x: x['attributes']['transactions'][0], last_registered_transactions))
+    saved_ids = list(filter(lambda x: x is not None, map(itemgetter('internal_reference'), last_registered_transactions)))
+    last_day = max(map(lambda x: datetime.fromisoformat(x['date']), last_registered_transactions))
+
+    new_transactions = n26_api.get_transactions(from_time=int(last_day.timestamp()*1000),
+                                                to_time=int(datetime.now().timestamp()*1000))
+    category_map = {c['id']: c['name'] for c in n26_api.get_available_categories()}
+    for t in filter(lambda x: x['id'] not in saved_ids and not x['pending'], new_transactions):
+        t = map_transaction(t, category_map)
+        firefly_api.create_transaction(t)
+    print()
+    # print(n26_api.get_transactions())
